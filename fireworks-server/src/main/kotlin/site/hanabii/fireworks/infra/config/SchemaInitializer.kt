ae8077fa4ffbd555e9c72d7ed3035b679e1c2b96
chuhao <cbaymax@qq.com>
2026-05-02 18:08:56 +0800

    feat(family): 注册 FamilyTree/PersonSpouse/FamilyLineage DDL，新增 FAMILY_TREE_NOT_FOUND 错误码

--- fireworks-server/src/main/kotlin/site/hanabii/fireworks/infra/config/SchemaInitializer.kt
+++ fireworks-server/src/main/kotlin/site/hanabii/fireworks/infra/config/SchemaInitializer.kt
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component
 import site.hanabii.fireworks.infra.UserDO
 import site.hanabii.fireworks.infra.auth.AccountDO
 import site.hanabii.fireworks.infra.vault.PasswordEntryDO
+import site.hanabii.fireworks.infra.family.FamilyPersonDO
+import site.hanabii.fireworks.infra.family.FamilyTreeDO
+import site.hanabii.fireworks.infra.family.PersonSpouseDO
+import site.hanabii.fireworks.infra.family.FamilyLineageDO
 import site.hanabii.fireworks.infra.vault.VaultConfigDO
 import javax.sql.DataSource
 
@@ -28,6 +32,10 @@ class SchemaInitializer(
                 stmt.execute(VaultConfigDO.DDL.trimIndent())
                 stmt.execute(PasswordEntryDO.DDL.trimIndent())
                 stmt.execute(AccountDO.DDL.trimIndent())
+                stmt.execute(FamilyPersonDO.DDL.trimIndent())
+                stmt.execute(FamilyTreeDO.DDL.trimIndent())
+                stmt.execute(PersonSpouseDO.DDL.trimIndent())
+                stmt.execute(FamilyLineageDO.DDL.trimIndent())
             }
         }
     }
